27279fd5264e1c93a0ddcf77643a9af730b22af4
Tom GERNEZ <tgernez@student.42.fr>
2024-07-12 01:10:41 +0200

    Printers

--- nmap/src/scanner.c
+++ nmap/src/scanner.c
@@ -46,11 +46,66 @@ void sigint_handler() {
     exit(1);
 }
 
+void print_ip_header(ipheader_t iph) {
+    printf("=============================IP  HEADER=============================\n");
+    printf("IP Version: %d\n", iph.ver);
+    printf("IP Header Length: %d\n", iph.ihl);
+    printf("IP Type Of Service: %c\n", iph.tos);
+    printf("IP Total Length: %d\n", iph.len);
+    printf("IP Identification: %d\n", iph.ident);
+    printf("IP Offset: %d\n", iph.offset);
+    printf("IP Flag: %d\n", iph.flag);
+    printf("IP Time To Live: %d\n", iph.ttl);
+    printf("IP Protocol: %d\n", iph.protocol);
+    printf("IP Checksum: %d\n", iph.chksum);
+    printf("IP Source IP: %u\n", iph.sourceip); // TODO convert me
+    printf("IP Destination IP: %u\n", iph.destip); // TODO convert me
+}
+
+void print_tcp_flags(unsigned char flags) {
+    int ind = 1;
+    const char *flag_names[9] = {
+        "FIN",
+        "SYN",
+        "RST",
+        "PSH",
+        "ACK",
+        "URG",
+        "ECE",
+        "CWR",
+        "NS"
+    };
+    
+    printf("TCP Flags:\n");
+    for (int i = 0; i < 9; i++) {
+        if (ind & flags)
+            printf("- Flag %s\n", flag_names[i]);
+        ind <<= 1;
+    }
+}
+
+
+void print_tcp_header(tcpheader_t tcph) {
+    printf("=============================TCP HEADER=============================\n");
+    printf("TCP Source Port: %hu\n", tcph.srcport);
+    printf("TCP Destination Port: %hu\n", tcph.destport);
+    printf("TCP Sequence Number: %u\n", tcph.seqnum);
+    printf("TCP Acknowledgment Number: %u\n", tcph.acknum);
+    printf("TCP Data Offset: %u\n", tcph.offset);
+    printf("TCP Reserved: %d\n", tcph.reserved);
+    print_tcp_flags(tcph.flags);
+    printf("TCP Window Size: %hu\n", tcph.win);
+    printf("TCP Checksum: %hu\n", tcph.chksum);
+    printf("TCP Urgent Pointer: %hu\n", tcph.urgptr);
+}
 
 int main() {
     int sockfd;
     // int results[PORTS_SCANNED];
-    char buff[BUFFER_SIZE];
+    char *buff = malloc(BUFFER_SIZE);
+    if (!buff)
+        return 1;
+
 
     sockfd = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
     if (sockfd < 0) {
@@ -64,9 +119,16 @@ int main() {
     ssize_t recvfrom_bytes = recvfrom(sockfd, buff, BUFFER_SIZE, 0, NULL, NULL);
     if (recvfrom_bytes > 0) {
         ipheader_t *iph = (ipheader_t *)buff;
+        // Print the data in iph
+        print_ip_header(*iph);
+
         tcpheader_t *tcph = (tcpheader_t *)(buff + 4 * iph->ihl);
+        // Print the data in tcph
+        print_tcp_header(*tcph);
+
+        // TODO maybe check if UDP?
         (void)tcph;
-        // &buff = &buff + sizeof(tcpheader_t);
+        buff = buff + sizeof(ipheader_t) + sizeof(tcpheader_t);
         printf("%s\n%ld\n", buff, recvfrom_bytes);
     }
 }
