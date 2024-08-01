201ccdf1845a6a2ab57fca6bd817e312167ff318
Tag6362 <tag6362@gmail.com>
2024-08-01 18:54:43 +0200

    IP header identifier made random

--- nmap/src/setup.c
+++ nmap/src/setup.c
@@ -20,7 +20,7 @@ ipheader_t setup_iph(int src_ip, int dest_ip, int data_len, int protocol) {
     } else if (protocol == IPPROTO_UDP) {
         iph.len = htons(sizeof(ipheader_t) + sizeof(udpheader_t) + data_len);
     }
-	iph.ident = htons(54321); // TODO make me random
+	iph.ident = htons(random_uint16(0, UINT16_MAX)); // TODO make me random
     iph.flag = htons(0x4000); // TODO study me
     iph.offset = 0; // TODO study me
     iph.ttl = 255; // TODO experiment with variable ttl for --traceroute param
